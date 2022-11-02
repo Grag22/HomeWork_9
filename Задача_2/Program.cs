bcb072d8a9fe435d689c8b097d4f1c1f390b8b0f
Кушнаренко Григорий <Gr4n777.yandex.ru>
2022-11-02 22:05:17 +0300

    Homework_9

---
+++ Задача_2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+void SumTwoNumber(int NumM, int NumN)
+{
+    int sum = 0;
+    for (int i = NumM; i <= NumN; i++)
+    {
+        sum = sum + NumM;
+        NumM++;
+    }
+    Console.WriteLine(sum);
+}
+
+Console.Write("Введите число M: ");
+int NumM = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int NumN = Convert.ToInt32(Console.ReadLine());
+
+SumTwoNumber(NumM, NumN);
